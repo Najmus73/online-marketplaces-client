45f74e59c565cbd8b041f7df9f3351b18da585f8
Najmus73 <shakibnajmus73@gmail.com>
2023-11-06 11:44:10 +0600

    Fetch Digital Marketing data on Digital Marketing tab from mongodb

---
+++ src/components/DigitalMarketing/DigitalMarketing.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+const DigitalMarketing = () => {
+    const [Jobs, setJobs] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/jobs')
+            .then(res => res.json())
+            .then(data => setJobs(data))
+    }, [])
+
+    const filter = Jobs.filter(job => job.category == 'Digital Marketing')
+
+    return (
+        <div>
+            {
+                filter.map(job =>
+                    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                        <figure><img src={job.image} alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{job.jobTitle}</h2>
+                            <p>{job.description}</p>
+                            <h1 className="font-bold">DeadLine : {job.deadline}</h1>
+                            <p className="text-lg font-bold text-blue-400">Price Range : ${job.minimumPrice} to ${job.maximumPrice}</p>
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+export default DigitalMarketing;
--- src/components/ReactTabs/ReactTabs.jsx
+++ src/components/ReactTabs/ReactTabs.jsx
@@ -1,26 +1,29 @@
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import WebDevelopment from '../WebDevelopment/WebDevelopment';
+import DigitalMarketing from '../DigitalMarketing/DigitalMarketing';
 const ReactTabs = () => {
     return (
         <div className='w-[1450px] mx-auto'>
-            <Tabs>            
-                    <TabList className="border-[2px] border-blue-400 flex justify-between gap-[50px] px-[200px] py-[8px] rounded-lg">
-                        <Tab className="font-bold text-blue-400">Web development</Tab>
-                        <h1 className="border-l-2 border-blue-400"></h1>
-                        <Tab className="font-bold text-blue-400">Digital marketing</Tab>
-                        <h1 className="border-l-2 border-blue-400"></h1>
-                        <Tab className="font-bold text-blue-400">Graphics design</Tab>
-                    </TabList>              
-                <TabPanel className='grid grid-cols-3 pt-[50px] h-[600px]'>
-                    <WebDevelopment></WebDevelopment>
-                </TabPanel>
-                <TabPanel>
-                    <h2>Any content 2</h2>
-                </TabPanel>
-                <TabPanel>
-                    <h2>Any content 3</h2>
-                </TabPanel>
+            <Tabs>
+                <TabList className="border-[2px] border-blue-400 flex justify-between gap-[50px] px-[200px] py-[8px] rounded-lg">
+                    <Tab className="font-bold text-blue-400">Web development</Tab>
+                    <h1 className="border-l-2 border-blue-400"></h1>
+                    <Tab className="font-bold text-blue-400">Digital marketing</Tab>
+                    <h1 className="border-l-2 border-blue-400"></h1>
+                    <Tab className="font-bold text-blue-400">Graphics design</Tab>
+                </TabList>
+                <div className='h-[700px]'>
+                    <TabPanel className='grid grid-cols-3 pt-[50px]'>
+                        <WebDevelopment></WebDevelopment>
+                    </TabPanel>
+                    <TabPanel>
+                        <DigitalMarketing></DigitalMarketing>
+                    </TabPanel>
+                    <TabPanel>
+                        <h2>Any content 3</h2>
+                    </TabPanel>
+                </div>
             </Tabs>
         </div>
     )
