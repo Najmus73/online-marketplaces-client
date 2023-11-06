8c9e1cb76afd083a4a86433bd145e65b5a5bcff5
Najmus73 <shakibnajmus73@gmail.com>
2023-11-06 12:01:46 +0600

    Fetch Graphics Design data on Graphics Design tab from mongodb

--- src/components/ReactTabs/ReactTabs.jsx
+++ src/components/ReactTabs/ReactTabs.jsx
@@ -2,6 +2,7 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import WebDevelopment from '../WebDevelopment/WebDevelopment';
 import DigitalMarketing from '../DigitalMarketing/DigitalMarketing';
+import GraphicsDesign from '../GraphicsDesign/GraphicsDesign';
 const ReactTabs = () => {
     return (
         <div className='w-[1450px] mx-auto'>
@@ -13,15 +14,15 @@ const ReactTabs = () => {
                     <h1 className="border-l-2 border-blue-400"></h1>
                     <Tab className="font-bold text-blue-400">Graphics design</Tab>
                 </TabList>
-                <div className='h-[700px]'>
-                    <TabPanel className='grid grid-cols-3 pt-[50px]'>
+                <div className='py-[50px]'>
+                    <TabPanel className='grid grid-cols-3'>
                         <WebDevelopment></WebDevelopment>
                     </TabPanel>
                     <TabPanel>
                         <DigitalMarketing></DigitalMarketing>
                     </TabPanel>
                     <TabPanel>
-                        <h2>Any content 3</h2>
+                        <GraphicsDesign></GraphicsDesign>
                     </TabPanel>
                 </div>
             </Tabs>
